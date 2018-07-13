2bce2eeffe23c235694626aa9cb8be2d32350a70
Ivam Galvão Filho <ivam.developer@gmail.com>
2018-07-12 23:38:29 -0300

    new_components_1

---
+++ app-devMaker/src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './views/pages/home/home.component';
+import { RegisterComponent } from './views/pages/register/register.component';
+
+const routes: Routes = [
+  {
+    path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+  },
+  {
+      path: 'home',
+      component: HomeComponent
+  },
+  {
+      path: 'register',
+      component: RegisterComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ app-devMaker/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavComponent } from './views/nav/nav.component';
+import { FooterComponent } from './views/footer/footer.component';
+import { HomeComponent } from './views/pages/home/home.component';
+import { RegisterComponent } from './views/pages/register/register.component';
+import { SideComponent } from './views/side/side.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavComponent,
+    FooterComponent,
+    HomeComponent,
+    RegisterComponent,
+    SideComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
