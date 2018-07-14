393410b0aa9745549bbf3ec645ba8d011c7b185f
Ivam Galvão Filho <ivam.developer@gmail.com>
2018-07-14 12:36:16 -0300

    Integration_api_cep_3

--- app-devMaker/src/app/views/pages/register/register.component.ts
+++ app-devMaker/src/app/views/pages/register/register.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Http } from '@angular/http';
 import { AppService } from '../../../app.service';
 import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { map } from 'rxjs/operators';
 
 
 @Component({
@@ -31,11 +32,42 @@ export class RegisterComponent implements OnInit {
 
   ngOnInit() { }
   onGetCep() {
-    return this.httpAppService.getCep()
+    const cep = this.form_reg.get('cep').value;
+    this.cep = cep.replace(/\D/g, '');
+    if (cep !== '') {
+      const validacep = /^[0-9]{8}$/;
+        if (validacep.test(cep)) {
+          this.reset();
+    return this.http.get(`https://viacep.com.br/ws/${cep}/json/`)
+    .pipe(map(data => data.json()))
     .subscribe(
         data => this.getData = data,
         error => alert(error),
         () => console.log(this.getData)
      );
+    }
+  }
+  }
+  reset() {
+    this.form_reg.patchValue(
+      {
+        rua: this.getData.rua,
+        num: this.getData.num,
+        comp: this.getData.comp,
+        uf: this.getData.uf,
+        cid: this.getData.cid
+      }
+    );
+  }
+  populaDados() {
+    this.form_reg.patchValue(
+      {
+        rua: null,
+        num: null,
+        comp: null,
+        uf: null,
+        cid: null
+      }
+    );
   }
 }
